e9bc108044d336cb170ef15d754a1240f0041200
Janier Rodríguez <jrodriguez@virtualcable.es>
2026-08-19 04:21:58 +0200

    Add smartcard redirection to the embedded RDP transport

--- src/uds/transports/RDPEmbedded/tunnel.py
+++ src/uds/transports/RDPEmbedded/tunnel.py
@@ -85,6 +85,7 @@ class TRDPEmbeddedTransport(BaseRDPEmbeddedTransport):
     webcam_fps = BaseRDPEmbeddedTransport.webcam_fps
     webcam_max_width = BaseRDPEmbeddedTransport.webcam_max_width
     webcam_max_height = BaseRDPEmbeddedTransport.webcam_max_height
+    enable_smartcard = BaseRDPEmbeddedTransport.enable_smartcard
 
     screen_size = BaseRDPEmbeddedTransport.screen_size
 
